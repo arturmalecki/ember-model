afb245e8235e523097e93000a80c5b7494ebebc2
Artur Małecki <seeweer@gmail.com>
2013-07-24 10:50:52 +0200

    Added failing test.
    https://github.com/ebryn/ember-model/issues/119

--- packages/ember-model/tests/adapter/custom_adapter_test.js
+++ packages/ember-model/tests/adapter/custom_adapter_test.js
@@ -17,3 +17,31 @@ test("throws an error message with class name", function() {
     Ember.run(CustomModel, CustomModel.find(1));
   }, /Ember.CustomAdapter must implement find/);
 });
+
+test("throws an error on hasMany relationship", function() {
+  expect(1);
+
+  var Post, Comment, record;
+
+  Post = Ember.Model.extend({
+    id: Ember.attr(),
+    name: Ember.attr(),
+    comments: Ember.hasMany("Comment")
+  });
+  Post.adapter = Ember.FixtureAdapter.create();
+  Post.FIXTURES = [{id: 1, name: 'Comment'}];
+
+  Comment = Ember.Model.extend({
+    name: Ember.attr()
+  });
+  Comment.adapter = Ember.CustomAdapter.create();
+
+  record = Ember.run(Post, Post.find, 1);
+  stop();
+  record.on('didLoad', function() {
+    start();
+    throws(function() {
+      Ember.run(record, record.get, 'comments');
+    }, /Ember.CustomAdapter must implement findAll/);
+  });
+});
